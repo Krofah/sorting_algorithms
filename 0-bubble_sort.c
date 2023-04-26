3e37c67bbd55a0c5c7fcc514b66d7972e663a465
Krofah <amponsahkrofah@gmail.com>
2023-04-25 17:08:14 -0700

    task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,14 +1,28 @@
 #include "sort.h"
 
-/**
-  * bubble_sort - Sorts an array of integers in ascending
-  * order using the Bubble sort algorithm.
-  * @array: The array of integers to sort
-  * @size: The size of the array of integers
-  *
-  * Description: Krofah Project
-  */
+#include "sort.h"
 
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap correctly.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
